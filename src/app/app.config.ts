dab0e13f3e1276d1fcd9c267abb9f98523a3889d
Laia Colomer <posets92@gmail.com>
2026-03-22 11:21:45 +0100

    feat(config): add catalan locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import localeCa  from '@angular/common/locales/ca';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localeCa);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'ca' }
+  ]
 };
