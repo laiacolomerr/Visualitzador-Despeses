659a423f9ef0b909218ffd7d2bacc345fda18381
Laia Colomer <posets92@gmail.com>
2026-03-22 12:40:33 +0100

    feat: show mock data on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { BarraCercaComponent } from "./components/barra-cerca/barra-cerca.component";
+import { LlistaElementsComponent } from "./components/llista-elements/llista-elements.component";
+import { PaymentMethod } from './models/payment-method.type';
+import { PAYMENTS } from './mocks/dades.mock';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    LlistaElementsComponent,
+    BarraCercaComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ioc-angular-VisualitzadorDespeses-LaiaColomer';
+  public payments = PAYMENTS;
+
+  getPaymentMethodLabel(paymentMethod: PaymentMethod): string {
+    switch (paymentMethod) {
+      case 'bankTransfer':
+        return 'Transferència';
+
+      case 'cash':
+        return 'Efectiu';
+      
+      case 'check':
+        return 'Xec';
+    
+      case 'creditCard':
+        return 'Targeta de crèdit';
+    
+      case 'debitCard':
+        return 'Targeta de dèbit';
+    
+      default:
+        return 'PayPal';
+    }
+  }
 }
