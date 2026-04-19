d86fe651119d0d4a2be3b39d44e302fd18cc6daf
Laia Colomer <posets92@gmail.com>
2026-04-19 09:28:31 +0200

    feat: add provideHttpClient provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import localeCa  from '@angular/common/locales/ca';
 
 import { routes } from './app.routes';
@@ -9,7 +10,9 @@ registerLocaleData(localeCa);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    { provide: LOCALE_ID, useValue: 'ca' }
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'ca' },
   ]
 };
