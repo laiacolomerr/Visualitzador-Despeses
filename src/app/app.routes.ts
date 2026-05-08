76d1075e3df57c47cd0816a8a9c8dc0ec2b45691
Laia Colomer <posets92@gmail.com>
2026-05-08 16:09:49 +0200

    feat: define application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LlistaPagamentsCercaComponent } from './pages/llista-pagaments-cerca/llista-pagaments-cerca.component';
+import { LlistaPagamentsComponent } from './pages/llista-pagaments/llista-pagaments.component';
+import { PagamentComponent } from './pages/pagament/pagament.component';
+import { PagamentPageComponent } from './pages/pagament-page/pagament-page.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'pagaments', pathMatch: 'full' },
+  { path: 'pagaments', component: LlistaPagamentsComponent, title: 'Catàleg de pagaments' },
+  { path: 'cerca', component: LlistaPagamentsCercaComponent, title: 'Cerca de pagaments' },
+  { path: 'destacats', component: PagamentPageComponent, title: 'Pagaments destacats' },
+  { path: 'detall/:id', component: PagamentComponent, title: 'Detall del pagament' },
+  { path: '**', redirectTo: 'pagaments' }
+];
