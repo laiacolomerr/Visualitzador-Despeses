f22288a78491818396ff1fc37d8bb7e0a6081d0b
Laia Colomer <posets92@gmail.com>
2026-05-08 17:26:01 +0200

    refactor: move navigation menu to a dedicated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { BarraMenuComponent } from "./components/barra-menu/barra-menu.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    BarraMenuComponent,
     RouterModule,
 ],
   templateUrl: './app.component.html',
---
+++ src/app/components/barra-menu/barra-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'barra-menu',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './barra-menu.component.html',
+  styleUrl: './barra-menu.component.scss'
+})
+export class BarraMenuComponent {
+
+}
